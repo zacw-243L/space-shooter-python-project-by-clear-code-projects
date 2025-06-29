954cff2521bf800875a31d53b814823c0c880df5
zacw-243L <qwerty.amousy123442x@gmail.com>
2025-06-29 16:44:27 +0800

    Stage 2: Add a surface to use sprites

--- code/main.py
+++ code/main.py
@@ -1,4 +1,6 @@
 import pygame
+from os.path import join
+from random import randint
 
 # general setup
 pygame.init()
@@ -7,14 +9,26 @@ display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption('Space shooter')
 running = True
 
+# plain surface
+surf = pygame.Surface((100, 200))
+surf.fill('orange')
+x = 100
+
+# importing images from parent directory's images folder
+player_surf = pygame.image.load(join('..', 'images', 'player.png')).convert_alpha()
+star_surf = pygame.image.load(join('..', 'images', 'star.png')).convert_alpha()
+star_positions = [(randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)) for _ in range(20)]
+
 while running:
-    # event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    # draw the game
-    display_surface.fill('dodgerblue1')
+    display_surface.fill('darkgray')
+    for pos in star_positions:
+        display_surface.blit(star_surf, pos)
+    x += 0.1
+    display_surface.blit(player_surf, (x, 150))
     pygame.display.update()
 
 pygame.quit()
