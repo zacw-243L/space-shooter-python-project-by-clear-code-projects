b3bed462a55210bb0360e423fbb8d6d2125bd233
zacw-243L <qwerty.amousy123442x@gmail.com>
2025-06-29 16:54:51 +0800

    Stage 3: Add a rects to store rectangular coordinates

--- code/main.py
+++ code/main.py
@@ -16,19 +16,38 @@ x = 100
 
 # importing images from parent directory's images folder
 player_surf = pygame.image.load(join('..', 'images', 'player.png')).convert_alpha()
+player_rect = player_surf.get_rect(center=(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+player_direction = -1
+
 star_surf = pygame.image.load(join('..', 'images', 'star.png')).convert_alpha()
 star_positions = [(randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)) for _ in range(20)]
 
+meteor_surf = pygame.image.load(join('..', 'images', 'meteor.png')).convert_alpha()
+meteor_rect = meteor_surf.get_rect(center=(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+
+laser_surf = pygame.image.load(join('..', 'images', 'laser.png')).convert_alpha()
+laser_rect = laser_surf.get_rect(bottomleft=(20, WINDOW_HEIGHT - 20))
+
 while running:
+    # event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+    # draw the game
     display_surface.fill('darkgray')
     for pos in star_positions:
         display_surface.blit(star_surf, pos)
-    x += 0.1
-    display_surface.blit(player_surf, (x, 150))
+
+    display_surface.blit(meteor_surf, meteor_rect)
+    display_surface.blit(laser_surf, laser_rect)
+
+    # player movement
+    player_rect.x += player_direction * 0.4
+    if player_rect.right > WINDOW_WIDTH or player_rect.left < 0:
+        player_direction *= -1
+    display_surface.blit(player_surf, player_rect)
+
     pygame.display.update()
 
 pygame.quit()
